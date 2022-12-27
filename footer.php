23b4b8f1b9e9e2bbca1b54a649e6ca4a00b71ecf
Thomas Johnson <53288963+ThomasSolidify@users.noreply.github.com>
2022-12-27 13:07:43 +0000

    Video and theme files

---
+++ footer.php
@@ -0,0 +1,26 @@
+<?php
+
+
+?>
+
+	<footer id="colophon" class="site-footer">
+		<div class="site-info">
+			<a href="<?php echo esc_url( __( 'https://wordpress.org/', 'bespoke' ) ); ?>">
+				<?php
+				/* translators: %s: CMS name, i.e. WordPress. */
+				printf( esc_html__( 'Proudly powered by %s', 'bespoke' ), 'WordPress' );
+				?>
+			</a>
+			<span class="sep"> | </span>
+				<?php
+				/* translators: 1: Theme name, 2: Theme author. */
+				printf( esc_html__( 'Theme: %1$s by %2$s.', 'Bespoke_USEDUPVC' ), 'Bespoke_USEDUPVC', '<a>Thomas Johnson</a>' );
+				?>
+		</div><!-- .site-info -->
+	</footer><!-- #colophon -->
+</div><!-- #page -->
+
+<?php wp_footer(); ?>
+
+</body>
+</html>
